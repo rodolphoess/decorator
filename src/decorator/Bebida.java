3a3028ab477286783834dc0a3a655e8779f41f1a
Rodolpho Erick <rodolphoerick90@gmail.com>
2018-11-13 10:41:27 -0300

    Inicio implementacao.

---
+++ src/decorator/Bebida.java
@@ -0,0 +1,15 @@
+package decorator_imp;
+
+public abstract class Bebida {
+	protected String descricao;
+	
+	protected Bebida() {
+		this.descricao = "";
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	abstract public double getPreco();
+}
